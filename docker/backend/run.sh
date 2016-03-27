ac6c89f683578588e92a9adc84df1f569482c02e
Rob <rob@spring-2885.org>
2016-03-27 11:39:43 -0700

    work on linking docker containers. This is so the frontend and backend can talk together.

--- docker/backend/run.sh
+++ docker/backend/run.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+set -e
+
+HOST_MYSQL_IP=$(hostname -I | awk '{print $1}')
+echo "Local IP address: ${HOST_MYSQL_IP}"
+
+echo "Starting docker backend container"
+
 docker run -it \
+  --name spring2885-backend \
   -v /home/rcleveng/secure/application-prod.properties:/config/application-prod.properties \
   -e SPRING_PROFILES_ACTIVE=prod \
-  --net=host \
+  --add-host=mysql:${HOST_MYSQL_IP} \
+  -p 8080:8080 \
   spring2885-backend
--- docker/frontend/run-httpd-foreground.sh
+++ docker/frontend/run-httpd-foreground.sh
@@ -1,1 +1,8 @@
-docker run -it --rm -p 80:80 -p 443:443 -v /etc/apache2/certs:/usr/local/apache2/certs spring2885-frontend
+docker run \
+       -it \
+       --rm \
+       -p 80:80 \
+       -p 443:443 \
+       -v /etc/apache2/certs:/usr/local/apache2/certs \
+       --link spring2885-backend:backend \
+       spring2885-frontend
