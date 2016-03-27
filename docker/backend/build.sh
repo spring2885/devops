0a43941a0931d1ee10b7f9cc8aa27d324924d8cb
Rob <rob@spring-2885.org>
2016-03-27 12:08:36 -0700

    build backend before packaging it. remove backend docker image when stopping it. It's ephemeral.

--- docker/backend/build.sh
+++ docker/backend/build.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+set -e
+
+# Build the backend jar.
+pushd backend/server
+./mvnw package
+popd
+
+# Build the docker image.
 docker build -t spring2885-backend .
--- docker/backend/run.sh
+++ docker/backend/run.sh
@@ -7,6 +7,7 @@ echo "Local IP address: ${HOST_MYSQL_IP}"
 echo "Starting docker backend container"
 
 docker run -it \
+  --rm \
   --name spring2885-backend \
   -v /home/rcleveng/secure/application-prod.properties:/config/application-prod.properties \
   -e SPRING_PROFILES_ACTIVE=prod \
